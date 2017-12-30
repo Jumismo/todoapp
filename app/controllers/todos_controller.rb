3b86d9bb67bf820c6d16a579fe528b5a2555e06d
Juan Miguel Sánchez Begines <jumisanbeg@gmail.com>
2017-12-30 17:36:45 +0000

    Remove redundancies and refactor code

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,11 +1,11 @@
 class TodosController < ApplicationController
+  before_action :set_todo, only: [:edit, :update, :show, :destroy]
   
   def new
     @todo = Todo.new
   end
   
   def create
-    @todo = Todo.new(todo_params)
     if @todo.save
       flash[:notice] = "Todo was created successfully!"
       redirect_to todo_path(@todo)
@@ -15,15 +15,12 @@ class TodosController < ApplicationController
   end
   
   def show
-    @todo = Todo.find(params[:id])
   end
   
   def edit
-    @todo = Todo.find(params[:id])
   end
   
   def update
-    @todo = Todo.find(params[:id])
     if @todo.update(todo_params)
       flash[:notice] = "Todo was successfully updated"
       redirect_to todo_path(@todo)
@@ -44,7 +41,10 @@ class TodosController < ApplicationController
   end
   
   private
-    
+  
+    def set_todo
+      @todo = Todo.find(params[:id])
+    end
     def todo_params
       params.require(:todo).permit(:name, :description)
     end
